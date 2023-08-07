bd36a8135e3081c67ded4c76686cdf5bf94912c2
Pon Vignesh <ponvigneshk@outlook.com>
2023-08-07 18:05:06 +0530

    POC-commit

---
+++ tests/conftest.py
@@ -0,0 +1,63 @@
+import allure
+import pytest
+from allure_commons.types import AttachmentType
+from selenium import webdriver
+from utilities import ReadConfigurations
+import mysql.connector
+import os, re
+import json
+
+@pytest.fixture()
+def log_on_failure(request):
+    yield
+    item = request.node
+    if item.rep_call.failed:
+        allure.attach(driver.get_screenshot_as_png(), name="failed_test",
+                      attachment_type=AttachmentType.PNG)
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
+
+class DBConnection:
+    def __init__(self):
+        with open(os.getcwd() + '\\EnvDatas\\DbConfigData.json', 'r') as js:
+            self.dbjsondata = json.load(js)
+        js.close()
+
+    def establishDBConnection(self):
+        try:
+            password = os.getenv("dbUserPassword")
+            db = mysql.connector.connect(host=self.dbjsondata['DbCredentials']['HostName'],
+                                         user=self.dbjsondata['DbCredentials']['DbUsername'], passwd=password)
+            return db
+        except mysql.connector.Error as e:
+            print(e)
+            return None
+
+@pytest.fixture()
+def setup_and_teardown(request):
+    browser = ReadConfigurations.read_configuration("basic info","browser")
+    global driver
+    driver = None
+    if browser.__eq__("chrome"):
+        driver = webdriver.Chrome()
+    elif browser.__eq__("firefox"):
+        driver = webdriver.Firefox()
+    elif browser.__eq__("edge"):
+        driver = webdriver.Edge()
+    else:
+        print("Provide a valid browser name from this list chrome/firefox/edge")
+    driver.maximize_window()
+    app_url = ReadConfigurations.read_configuration("basic info","url")
+    driver.get(app_url)
+    request.cls.driver = driver
+    yield
+    driver.quit()
+
+
+
